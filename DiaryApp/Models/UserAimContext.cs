c7214f3871c681a8c90ed74e315fdaf59520e416
Olga Kul <helgaukrop@gmail.com>
2019-04-29 23:04:13 +0300

    Add data context

---
+++ DiaryApp/Models/UserAimContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace DiaryApp.Models
+{
+    public class UserAimContext:DbContext
+    {
+        public DbSet<UserAim> UserAims { get; set; }
+
+        public UserAimContext(DbContextOptions<UserAimContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+
+
+    }
+}
---
+++ DiaryApp/Models/UserTaskContext.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DiaryApp.Models
+{
+    public class UserTaskContext : DbContext
+    {
+        public DbSet<UserTask> UserTasks { get; set; }
+        public DbSet<Period> Periods { get; set; }
+        public DbSet<Priority> Priorities { get; set; }
+        public DbSet<DailyTasks> DailyTasks { get; set; }
+        public DbSet<WeeklyTasks> WeeklyTasks { get; set; }
+        public DbSet<MonthlyTasks> MonthlyTasks { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+        public UserTaskContext (DbContextOptions<UserTaskContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+    }
+}
